16851e0021d595ad16a84cc680736994db328c4c
genn125 <genn125@gmail.com>
2025-02-13 19:53:57 +0300

    Сокращенный цикл for in

--- 6.py
+++ 6.py
@@ -4,15 +4,9 @@
 qwe = {'нижний': 'Пофигу', 'РазННыЙ': 'что тут', 'РЕГИСТР': 'написано'}
 qwer = ['нижний', '12', 'РазННыЙ', 'что', 'РЕГИСТР', 'O']
 
-# qwe1 = {}
-# for k, v in qwe.items():
-# qwe1.update({k:v.upper()})
+
 qwe1 = {k: v.upper() for k, v in qwe.items()}
 
-# qwer1=[]
-# for i in qwer:
-#     if len(i) > 3:
-#         qwer1.append(i)
 qwer1 = [i for i in qwer if len(i) > 3]
 
 print('1 - ', qwe)
@@ -20,3 +14,12 @@ print('2 - ', qwe1)
 
 print('3 - ', qwer)
 print('4 - ', qwer1)
+
+# qwe1 = {}
+# for k, v in qwe.items():
+# qwe1.update({k:v.upper()})
+
+# qwer1=[]
+# for i in qwer:
+#     if len(i) > 3:
+#         qwer1.append(i)
