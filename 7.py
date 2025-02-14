7d0d44c3bee9b90032f8a58a327cabb4da86dce9
genn125 <genn125@gmail.com>
2025-02-14 11:24:37 +0300

    Классы и экземпляры

--- 7.py
+++ 7.py
@@ -2,10 +2,26 @@
 # Классы и экземпляры
 
 class Image:
+
     def __init__(self, resolution, title, extension):
         self.resolution = resolution
         self.title = title
         self.extension = extension
+
+    def super_print(f):
+        m = [0]
+        foo = print
+
+        def helper(x):
+            m[0] = f(m[0])
+            foo(f'[{m[0]}]: {x}')
+
+        return helper
+
+    @super_print
+
+    def print(n):
+        return n + 1
 
     def resize (self, res):
         self.resolution = res
@@ -13,15 +29,34 @@ class Image:
     def __str__(self):
         return f'{self.title}.{self.extension}'
 
+    @staticmethod
+    def tyu (a,b):
+        return f'{a} {b}'
+
+def super_print(f):
+    m = [0]
+    foo = print
+
+    def helper(x):
+        m[0] = f(m[0])
+        foo(f'[{m[0]}]: {x}')
+
+    return helper
+
+@super_print
+def print(n):
+    return n + 1
+
 first_img = Image('200x400', 'fig1', 'jpg')
 
 print(first_img.resolution)
 print(first_img.title)
 print(first_img.extension)
-print()
+print("__________")
 first_img.resize('2x2')
 print(first_img.resolution)
 print(first_img)
+print(first_img.tyu(123, 567))
 
 zxc = Image('1920x1080', 'fig2', 'png')
 # zxc.resize('3840x2160')
---
+++ numerate.py
@@ -0,0 +1,27 @@
+
+
+def super_print(f):
+    m = [0]
+    foo = print
+
+    def helper(x):
+        m[0] = f(m[0])
+        foo(f'[{m[0]}]: {x}')
+
+    return helper
+
+@super_print
+def print(n):
+    return n + 1
+
+###################################
+
+def count_calls(func):
+    global wrapper
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        # print(f"Функция {func.__name__} вызвана {wrapper.count}-й раз")
+        return func(*args, **kwargs)
+    wrapper.count = 0
+    return wrapper
+@count_calls
