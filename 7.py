68762b76e13eb89471ae0c9b2ada97abd86a93b4
genn125 <genn125@gmail.com>
2025-02-14 11:50:19 +0300

    Классы и экземпляры

--- 7.py
+++ 7.py
@@ -18,34 +18,44 @@ class Image:
     def tyu (a,b):
         return f'{a} {b}'
 
-def super_print(f):
-    m = [0]
-    foo = print
 
-    def helper(x):
-        m[0] = f(m[0])
-        foo(f'[{m[0]}]: {x}')
 
-    return helper
-
-@super_print
-def print(n):
-    return n + 1
+#
+# def super_print(f):
+#     m = [0]
+#     foo = print
+#
+#     def helper(x):
+#         m[0] = f(m[0])
+#         foo(f'[{m[0]}]: {x}')
+#
+#     return helper
+#
+# @super_print
+# def print(n):
+#     return n + 1
 
 first_img = Image('200x400', 'fig1', 'jpg')
-
-print(first_img.resolution)
-print(first_img.title)
-print(first_img.extension)
-print("__________")
-first_img.resize('2x2')
-print(first_img.resolution)
-print(first_img)
-print(first_img.tyu(123, 567))
-
 zxc = Image('1920x1080', 'fig2', 'png')
-zxc.resize('3840x2160')
-print(zxc.resolution)
-print(zxc.title)
-print(zxc.extension)
-print(zxc)
+
+
+
+
+
+
+#
+# print(first_img.resolution)
+# print(first_img.title)
+# print(first_img.extension)
+# print("__________")
+# first_img.resize('2x2')
+# print(first_img.resolution)
+# print(first_img)
+# print(first_img.tyu(123, 567))
+#
+#
+# zxc.resize('3840x2160')
+# print(zxc.resolution)
+# print(zxc.title)
+# print(zxc.extension)
+# print(zxc)
