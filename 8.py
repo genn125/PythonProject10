ff706d933ce60357fa505fe3f607a75d6320d3c7
genn125 <genn125@gmail.com>
2025-02-15 14:38:36 +0300

    Магические методы в классах

--- 8.py
+++ 8.py
@@ -9,17 +9,25 @@ class Comment:
     def upvote(self):
         self.votes_qty += 1
 
-    def __add__(self, other, tr):
-        return f'{self.text} + {other.text} + {tr.text}', self.votes_qty + other.votes_qty + tr.votes_qty
+    def __add__(self, other):
+        return (f"{self.text} {other.text}", self.votes_qty + other.votes_qty)
 
 first_comment = Comment('Первый комментарий')
+first_comment.upvote()
+first_comment.upvote()
+first_comment.upvote()
 first_comment.upvote()
 
 second_comment = Comment('Второй комментарий')
 second_comment.upvote()
+second_comment.upvote()
 
-tr_comment = Comment('3 комментарий')
-tr_comment.upvote()
+                                   #       Как соединить 3 и более комментариев
 
-print(__add__.Comment)
-# print(first_comment + second_comment + tr_comment)
+three_comment = Comment('3 комментарий')
+three_comment.upvote()
+
+
+print(first_comment + second_comment)
+print(first_comment + three_comment)
+print(second_comment + three_comment)
