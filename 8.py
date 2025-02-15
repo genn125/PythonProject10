0aa83634b417739b062430a9fdad5057b2c1db33
genn125 <genn125@gmail.com>
2025-02-15 11:13:59 +0300

    Магические методы в классах

--- 8.py
+++ 8.py
@@ -2,15 +2,15 @@
 # Магические методы в классах
 
 class Comment:
-    def __init__(self, text):
+    def __init__(self, text,tr_comment):
         self.text = text
         self.votes_qty = 0
-
+        self.tr_comment=tr_comment
     def upvote(self):
         self.votes_qty += 1
 
-    def __add__(self, other):
-        return f'{self.text} + {other.text}', self.votes_qty + other.votes_qty
+    def __add__(self, other, tr_comment):
+        return f'{self.text} + {other.text}', self.votes_qty + other.votes_qty + tr_comment.votes_qty
 
 first_comment = Comment('Первый комментарий')
 first_comment.upvote()
