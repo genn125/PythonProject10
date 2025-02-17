b5bdb6e8a9c270340837b164b0ca96ba0b26d5e0
genn125 <genn125@gmail.com>
2025-02-18 00:30:54 +0300

    Работа с файлами

---
+++ 10.py
@@ -0,0 +1,15 @@
+# 45 Работа с файлами
+
+from pathlib import Path
+
+qwerty = Path('D:/') / 'Pycharm' / 'PythonProject10'/ 'Test_one'  # "тот" путь
+if not qwerty.exists():  # проверяет, есть ли такая папка (без этого, если есть папка, то выдаст ошибку)
+    qwerty.mkdir()       # Создание папки по "тому" пути
+
+with open('./Test_one/first.txt',"w",  encoding='utf-8') as test_file:
+    test_file.write('Привет, первая строчка\n')
+    test_file.write('Привет, вторая строчка\n')
+
+with open('./Test_one/second.txt',"w",  encoding='utf-8') as test_file:
+    test_file.write('Привет, я из second файла первая строчка\n')
+    test_file.write('Привет, я из second файла ВТОРАЯ строчка\n')
