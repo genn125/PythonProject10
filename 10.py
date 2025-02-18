663609fb91738d27f9b8c668bf019bf9c96f0dc8
genn125 <genn125@gmail.com>
2025-02-18 18:10:35 +0300

    Работа с файлами

--- 10.py
+++ 10.py
@@ -3,38 +3,44 @@
 from pathlib import Path
 
 qwerty = Path('D:/') / 'Pycharm' / 'PythonProject10'/ 'Test_one'  # "тот" путь
-if not qwerty.exists():  # проверяет, есть ли такая папка (без этого, если есть папка, то выдаст ошибку)
-    qwerty.mkdir()       # Создание папки по "тому" пути
+# if not qwerty.exists():  # проверяет, есть ли такая папка (без этого, если есть папка, то выдаст ошибку)
+qwerty.mkdir(exist_ok=True)       # Создание папки по "тому" пути с проверкой
 
-with open('./Test_one/first.txt',"w",  encoding='utf-8') as test_file:
-    test_file.write('Привет, первая строчка из first.txt\n')
-    test_file.write('Привет, вторая строчка из first.txt\n')
+first_file = qwerty / 'first.txt'
+second_file = qwerty / 'second.txt'
 
-with open('./Test_one/second.txt',"w",  encoding='utf-8') as test_file:
-    test_file.write('Привет, я из second файла первая строчка\nПривет, я из second файла ВТОРАЯ строчка\n'
-                    'Привет, я из second файла Третья строчка\n')
+with open(first_file,"w",  encoding='utf-8') as f:
+#  Записать строки в файл вар. 1
+    f.write('Привет, первая строчка из first.txt\n')
+    f.write('Привет, вторая строчка из first.txt\n')
 
-with open ('./Test_one/first.txt','r', encoding='utf-8') as test_file:
-    print(test_file.read())
+with open(second_file,"w",  encoding='utf-8') as f:
+#  Записать строки в файл вар. 2
+    lines = ['Привет, я из second файла первая строчка', 'Привет, я из second файла ВТОРАЯ строчка',
+                    'Привет, я из second файла Третья строчка']
+    for line in lines:
+        f.write(line + '\n')
 
-with open('./Test_one/second.txt', 'r', encoding='utf-8') as test_file:
-    # print(test_file.readline(100))    #  Работа с курсором, после прочтения первой строки он
-    # print(test_file.readline(10))     #  переходит на вторую и печатает 10 (что в ()) символов
-    # print(test_file.readlines())    # Печатает все строки в список
+with open (first_file, encoding='utf-8') as f:
+    print(f.read())
 
-    # for l in test_file:
-    #     print(l)
+with open(second_file, encoding='utf-8') as f:
+#  Вариант 1
+    for l in f:
+        print(l.strip())   # strip убирает пробелы в начале и в конце и \n
+# Вариант 2
+    # print(f.readline(100))    #  Работа с курсором, после прочтения первой строки он
+    # print(f.readline(10))     #  переходит на вторую и печатает 10 (что в ()) символов
+    # print(f.readlines())    # Печатает все строки в список
+#  Вариант 3
+    # while True:
+    #     l = f.readline()
+    #     if not l:
+    #         break
+    #     print(l.strip())
 
-    while True:
-        l = test_file.readline()
-        print(l)
-        if not l:
-            break
-
-my_file1 = Path('./Test_one/first.txt')
-my_file2 = Path('./Test_one/second.txt')
-if my_file1.exists() or my_file2.exists():   # проверяет, есть ли такой файл
-    my_file1.unlink()       #  Удаление ранее созданного файла
-    my_file2.unlink()
+if first_file.exists() or second_file.exists():   # проверяет, есть ли такой файл
+    first_file.unlink()       #  Удаление ранее созданного файла
+    second_file.unlink()
 
 qwerty.rmdir()
