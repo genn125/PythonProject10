9b9b6d8812487fc613b29368c1b5a0c9e840a749
genn125 <genn125@gmail.com>
2025-02-19 15:42:05 +0300

    Работа с регулярными выражениями (re)

--- 11.py
+++ 11.py
@@ -2,22 +2,40 @@
 
 import re
 
-input('Введите Пароль: ')
-
-
-
 def check_password (password):
-    length_pattern = re.compile(r"\s{8,}")    # Паттерн проверки длинны
-    lowercase_pattern = re.compile(r"[a-z]+") # Паттерн проверки малых букв ("+" означает, что обязательно должна быть)
-    uppercase_pattern = re.compile(r"[A-Z]+") # Паттерн проверки БОЛЬШИХ букв ("+" означает, что обязательно должна быть)
-    number_pattern = re.compile(r"[0-9]+") # Паттерн проверки цифр ("+" означает, что обязательно должна быть)
-    special_pattern = re.compile(r"[@|%#!&*^]+") # Паттерн проверки спецсимволов ("+" означает, что обязательно должна быть)
+    length_pattern = re.compile(r"\S{8,}")             # Паттерн проверки длинны
+    lowercase_pattern = re.compile(r"^.*[a-z]+.*$")    # Паттерн проверки малых букв ("+" означает, что обязательно должна быть)
+    uppercase_pattern = re.compile(r"^.*[A-Z]+.*$")    # Паттерн проверки БОЛЬШИХ букв ("+" означает, что обязательно должна быть)
+    number_pattern = re.compile(r"^.*[0-9]+.*$")       # Паттерн проверки цифр ("+" означает, что обязательно должна быть)
+    special_pattern = re.compile(r"^.*[@|%#!&*^]+.*$") # Паттерн проверки спецсимволов ("+" означает, что обязательно должна быть)
+    no_whitespace_pattern = re.compile(r"^\S*$")       # Паттерн проверки пробелов
+
+    if not re.fullmatch(no_whitespace_pattern, password):
+        return (False, 'Пароль не должен содержать пробелы')
+    if not re.fullmatch(length_pattern, password):
+        return (False, 'Пароль должен быть более 8 символов')
+    if not re.fullmatch(lowercase_pattern, password):
+        return (False, 'Пароль должен содержать малые буквы')
+    if not re.fullmatch(uppercase_pattern, password):
+        return (False, 'Пароль должен содержать БОЛЬШИЕ буквы')
+    if not re.fullmatch(number_pattern, password):
+        return (False, 'Пароль должен содержать цифры буквы')
+    if not re.fullmatch(special_pattern, password):
+        return (False, 'Пароль должен содержать спецсимволы буквы')
+    return (True, 'Пароль верный')
+
+while True:
+    password = input('Введите пароль: ')
+    res = check_password(password)
+    if res[0]:
+        print(res[1])
+        break
+    print(res[1])
 
 
 
-    password_regexp = r"^[a-zA-Z0-9]+$"
-    password_check_pattern = re.compile(password_regexp)
-    validation_resit = "Пароль установлен" if password_check_pattern.fullmatch(password) else "Введите правильный пароль"
-    return validation_resit
 
-print(check_password('d!!b'))
+
+
+
+
