43282eee10ce52bfce222cd4ead4ec1f045e34c2
genn125 <genn125@gmail.com>
2025-02-22 12:38:54 +0300

    Работа с базой данных SQLite

--- 12.py
+++ 12.py
@@ -9,17 +9,19 @@ DB_NAME = 'sqlite_db.db'
 #     print(sqlite3.version)
 
 with sqlite3.connect(DB_NAME) as sqlite_conn:
+# SQL запрос для создания таблицы
     sql_request = """CREATE TABLE IF NOT EXISTS courses ( 
     id integer PRIMARY KEY,
     title text NOT NULL,
     student_qty integer,
     reviews_qty integer
     );"""
+# CREATE TABLE IF NOT EXISTS - создание таблицы, если ее еще нет
 # courses - название таблицы
-# название полей и их тип
+# id integer, reviews_qty и тд - название полей и их тип
 # PRIMARY KEY - основной ключ, NOT NULL - обязательно должно быть заполнено
 
-    sqlite_conn.execute(sql_request)
+    sqlite_conn.execute(sql_request) # execute -  выполнение sql запроса
 
 
 
