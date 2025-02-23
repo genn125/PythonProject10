91ecdd41c4f7197edc24f126f4c9223156407332
genn125 <genn125@gmail.com>
2025-02-23 21:08:16 +0300

    Работа с базой данных SQLite

--- qqq.py
+++ qqq.py
@@ -4,35 +4,34 @@ import sqlite3
 connection = sqlite3.connect('tasks.db')
 
 cursor = connection.cursor()
+#
+# Tasks = "Музыкальные группы"
+# albums = "Альбомы"
+# compositions = "Композиции"
+# title = "Группа"
 
-Tasks = "Музыкальные группы"
-albums = "Альбомы"
-compositions = "Композиции"
-title = "Группа"
 
 
 # Создаем таблицу "Музыкальные группы"
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Tasks (
 № INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-albums TEXT,
-compositions TEXT)
+Группа TEXT NOT NULL,
+Альбом TEXT,
+Композиции TEXT)
 """)
 
 
 # Функция для добавления новой задачи
 def add_task(title):
-
-    cursor.execute('INSERT INTO Tasks (title) VALUES (?)', (title,))
+    cursor.execute('INSERT INTO Tasks ("Группа") VALUES (?)', (title,))
     connection.commit()
 
 
-# Функция для обновления albums
+# Функция для обновления Альбома
 def update_task_albums(task_id, albums):
-    cursor.execute('UPDATE Tasks SET albums = ? WHERE id = ?', (albums, task_id))
+    cursor.execute('UPDATE Tasks SET Альбом = ? WHERE № = ?', (albums, task_id))
     connection.commit()
-
 
 # Функция для вывода списка задач
 def list_tasks():
@@ -42,20 +41,24 @@ def list_tasks():
     for task in tasks:
         print(task)
 
-
 # Добавляем Группы
 while True:
     q = input('Введите название группы: ')
     if q=="":
         break
     add_task(q)
-    # continue
 
-
-# Обновляем статус задачи
-# update_task_status(2, 'In Progress')
+# Добавляем альбомы
+task_id = 1
+while task_id:
+    task_id += 1
+    q = input('Введите название альбома: ')
+    if q=="":
+        break
+    update_task_albums(task_id, q)
 
 # Выводим список задач
+
 list_tasks()
 
 # Закрываем соединение
