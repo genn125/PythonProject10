f6535b1ccec320481e7e8a2164d37db5f83a98c7
genn125 <genn125@gmail.com>
2025-02-25 21:08:02 +0300

    Работа с базой данных SQLite

--- 12.py
+++ 12.py
@@ -3,57 +3,51 @@
 import sqlite3
 
 DB_NAME = 'sqlite_db.db'
+try:
+    # Создание новой таблицы
+    with sqlite3.connect(DB_NAME) as sqlite_conn:
+    # SQL запрос для СОЗДАНИЯ таблицы
 
-# Создайте новую таблицу
-with sqlite3.connect(DB_NAME) as sqlite_conn:
-# SQL запрос для СОЗДАНИЯ таблицы
-    sql_request = """CREATE TABLE IF NOT EXISTS courses (
-    id integer PRIMARY KEY,
-    title text NOT NULL,
-    student_qty integer,
-    reviews_qty integer
-    )"""
+            sql_request = """CREATE TABLE IF NOT EXISTS courses (
+            id integer PRIMARY KEY,
+            title text NOT NULL,
+            student_qty integer,
+            reviews_qty integer
+        )"""
                          # CREATE TABLE IF NOT EXISTS - создание таблицы, если ее еще нет
                          # courses - название таблицы
                        # ide integer, reviews_qty и тд - название полей и их тип
                          # PRIMARY KEY - основной ключ, NOT NULL - обязательно должно быть заполнено
-    sqlite_conn.execute(sql_request) # execute -  выполнение sql запроса
-    sqlite_conn.commit()  # сохранение изменений в базе
-##################################################################
+            sqlite_conn.execute(sql_request) # execute -  выполнение sql запроса
+            sqlite_conn.commit()  # сохранение изменений в базе
+
+except sqlite3.Error as error:
+    print("Ошибка 1: ", error)
+    sqlite_conn.close()
+# ##################################################################
 
 # Добавление записей в таблицу
 courses =[(1,"Ява-скрипт", 20, 100),
           (2, "С++", 32, 50),
           (3, "Ява полный", 15, 35),
-          (4, "vvv", 2, 11),(5, "hhh", 5, 0),
-          (6, "ddd", 12, 5),
-          (7,'gg',66,7)
-          ]
+          (4, "vvv", 2, 11),
+        (5, "Ява полный", 15, 35),
+        (6, "Ява полный", 15, 35),
+          (7, "Ява полный", 15, 35),
+        ]
 
 with sqlite3.connect(DB_NAME) as sqlite_conn:
 # SQL запрос для ЗАПОЛНЕНИЯ таблицы
     try:
         for course in courses:
-            sqlite_conn.execute("INSERT INTO courses VALUES (?, ?, ?, ?)", course)   # execute - выполнение sql запроса с заполнением таблицы
+            # execute - выполнение sql запроса с заполнением таблицы
+            sqlite_conn.execute("INSERT INTO courses VALUES (?, ?, ?, ?)", course)
             sqlite_conn.commit()  # сохранение изменений в базе
         print("Данные успешно добавлены в базу данных.")
     except sqlite3.Error as error:
-        print("Некоторые данные уже есть в базе. Ошибка: ", error)
+        print("Ошибка 2: ", error)
+
 # #########################################################
-
-
-
-# with sqlite3.connect(DB_NAME) as sqlite_conn:
-#     sql_request ="SELECT id FROM courses"
-#     sql_cursor = sqlite_conn.execute(sql_request)
-#     for r in sql_cursor:
-#         if r[0] != True:
-#             sqlite_conn.execute("INSERT INTO courses VALUES (?, ?, ?, ?)",courses)  # execute - выполнение sql запроса с заполнением таблицы
-#             sqlite_conn.commit()  # сохранение изменений в базе
-#     print("Данные успешно добавлены в базу данных.")
-
-
-
 
 with sqlite3.connect(DB_NAME) as sqlite_conn:
 # SQL запрос для ЧТЕНИЯ таблицы
@@ -61,16 +55,6 @@ with sqlite3.connect(DB_NAME) as sqlite_conn:
     sql_cursor = sqlite_conn.execute(sql_request)      # execute - выполнение sql запроса
     for r in sql_cursor:    #  перебор и печать значений в виде кортежей
         print(r)
-
-
-
-
-
-
-
-
-
-
-    # r = sql_cursor.fetchall()   # печать значений в виде списка кортежей, если пустой, то курсор в конце
-    # print("Печать в виде списка кортежей невозможна, достигнут конец таблицы. Кортеж пустой -" , r)
+    r = sql_cursor.fetchall()   # печать значений в виде списка кортежей, если пустой, то курсор в конце
+    print("Печать в виде списка кортежей невозможна, достигнут конец таблицы. Кортеж пустой -" , r)
 
--- qqq.py
+++ qqq.py
@@ -1,65 +1,14 @@
 import sqlite3
 
-# Устанавливаем соединение с базой данных
-connection = sqlite3.connect('tasks.db')
+CREATE TABLE people (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER
+)
 
-cursor = connection.cursor()
-#
-# Tasks = "Музыкальные группы"
-# albums = "Альбомы"
-# compositions = "Композиции"
-# title = "Группа"
-
-
-
-# Создаем таблицу "Музыкальные группы"
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Tasks (
-№ INTEGER PRIMARY KEY,
-Группа TEXT NOT NULL,
-Альбом TEXT,
-Композиции TEXT)
-""")
-
-
-# Функция для добавления новой задачи
-def add_task(title):
-    cursor.execute('INSERT INTO Tasks ("Группа") VALUES (?)', (title,))
-    connection.commit()
-
-
-# Функция для обновления Альбома
-def update_task_albums(task_id, albums):
-    cursor.execute('UPDATE Tasks SET Альбом = ? WHERE № = ?', (albums, task_id))
-    connection.commit()
-
-# Функция для вывода списка задач
-def list_tasks():
-
-    cursor.execute('SELECT * FROM Tasks')
-    tasks = cursor.fetchall()
-    for task in tasks:
-        print(task)
-
-# Добавляем Группы
-while True:
-    q = input('Введите название группы: ')
-    if q=="":
-        break
-    add_task(q)
-
-# Добавляем альбомы
-task_id = 1
-while task_id:
-    task_id += 1
-    q = input('Введите название альбома: ')
-    if q=="":
-        break
-    update_task_albums(task_id, q)
-
-# Выводим список задач
-
-list_tasks()
-
-# Закрываем соединение
-connection.close()
+con = sqlite3.connect("metanit.db")
+cursor = con.cursor()
+# данные для добавления
+people = [("Sam", 28), ("Alice", 33), ("Kate", 25)]
+cursor.executemany("INSERT INTO people (name, age) VALUES (?, ?)", people)
+con.commit()
