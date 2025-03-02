0f565ef4b379cf122977aa034cef589cf49446c2
genn125 <genn125@gmail.com>
2025-03-02 23:07:51 +0300

    Декоратор: Проверка аргументов

--- 15.2 Декоратор Проверка аргументов.py
+++ 15.2 Декоратор Проверка аргументов.py
@@ -1,15 +1,13 @@
+# Декор: Проверка аргументов
 
 def dekorator(fn):
     def wrapper(*args, **kwargs):
         for arg in [*args, *kwargs.values()]:
-
             if not isinstance(arg, int) and not isinstance(arg, float):
                 raise ValueError(f'Тип {arg} является {type(arg)} . Аргументы должны быть целым числом или с запятой')
 # Почему после raise не продолжается работа
         return fn(*args, **kwargs)
-
     return wrapper
-
 @dekorator
 def sum_nums(a, b):
      return a + b
@@ -20,6 +18,5 @@ try:
     print(sum_nums(a=6.7, b='25'))           # Как продолжить вызовы функции?
     print(sum_nums(23.5,7.9))
 except ValueError as e:
-
     print(e)
 
--- numerate.py
+++ numerate.py
@@ -4,18 +4,15 @@ def super_print(f):
     m = [0]
     foo = print
 
-    def helper(x):
+
+a def helper(x):
         m[0] = f(m[0])
         foo(f'[{m[0]}]: {x}')
-
     return helper
-
 @super_print
 def print(n):
     return n + 1
-
 ###################################
-
 def count_calls(func):
     global wrapper
     def wrapper(*args, **kwargs):
