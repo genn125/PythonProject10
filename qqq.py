2680e48e76234b8caf95a8bfdd3cb3ed7821112d
genn125 <genn125@gmail.com>
2025-03-02 10:33:01 +0300

    Декораторы

--- qqq.py
+++ qqq.py
@@ -1,14 +1,23 @@
-import sqlite3
 
-CREATE TABLE people (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    age INTEGER
-)
+def dekorator(fn):
+    def wrapper(*args, **kwargs):
+        for arg in [*args, *kwargs.values()]:
+            if not isinstance(arg, int) and not isinstance(arg, float):
+                raise ValueError (f'Тип {arg} является {type(arg)} . Аргументы должны быть целым числом или с запятой')
+        return fn(*args, **kwargs)
 
-con = sqlite3.connect("metanit.db")
-cursor = con.cursor()
-# данные для добавления
-people = [("Sam", 28), ("Alice", 33), ("Kate", 25)]
-cursor.executemany("INSERT INTO people (name, age) VALUES (?, ?)", people)
-con.commit()
+    return wrapper
+
+
+@dekorator
+def sum_nums(a, b):
+    return a + b
+try:
+    print(sum_nums(7,2))
+    print(sum_nums(23.5,7.9))
+    print(sum_nums([1, 2, 3], '25'))
+    print(sum_nums(a=6.7, b='25'))
+    print(sum_nums(23.5,7.9))
+except ValueError as e:
+    print(e)
+
